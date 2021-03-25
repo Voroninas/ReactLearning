bb9d3764dd97c788443264509aa6a1d0ccf70384
Sorokinas <KunoMan1333@yandex.ru>
2021-03-25 20:36:56 +0300

    54 теперь у компоненты Users запрос прописан правильно, в componentDidMount

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -3,31 +3,13 @@ import styles from './Users.module.css'
 import * as axios from 'axios'
 import userPhoto from '../../assets/images/user.png'
 
-/*const Users = (props) => {*/
 class Users extends React.Component {
-	constructor(props) {
-		super(props)
-		//alert("запрос на сервер")
-		axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-			this.props.setUsers(response.data.items)
-		})
-		/*this.getUsers() // мой вариант*/
+	componentDidMount(){
+		axios.get('https://social-network.samuraijs.com/api/1.0/users')
+			.then(response => {
+				this.props.setUsers(response.data.items)
+			})
 	}
-	/*getUsers = () => { // мой вариант
-		axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-			this.props.setUsers(response.data.items)
-		})
-
-		this.props.setUsers([
-		{ id: 1, photoUrl: 'https://kto-chto-gde.ru/wp-content/uploads/2016/09/1423402942.jpg', 
-				followed: true, fullName: 'Sam', status: 'some status', location: { city: 'Moscow', country: 'Russia' } },
-		{ id: 2, photoUrl: 'https://www.zbrushcentral.com/uploads/default/original/4X/5/5/b/55b398dd2b0824ca07415adaac899bad13c5d739.jpeg', 
-				followed: false, fullName: 'Bob', status: 'some new status', location: { city: 'Minsk', country: 'Russia' } },
-		{ id: 3, photoUrl: 'https://awkward.com/wp-content/uploads/2019/03/Screen-Shot-2019-03-06-at-2.53.55-PM.jpg', 
-				followed: true, fullName: 'Tom', status: 'some another status', location: { city: 'Stalinsk', country: 'Russia' } }
-		]
-		)
-	}*/
 
 	render() {
 		return <div>
