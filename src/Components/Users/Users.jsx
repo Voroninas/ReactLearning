5e4147f7271a3347b6bdf15009d0683eaa222034
Sorokinas <KunoMan1333@yandex.ru>
2021-04-29 20:56:16 +0300

    62 новые запросы на сервер для follow/unfollow

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -34,8 +34,34 @@ const Users = (props) => {
           </div>
 					<div>
 						{u.followed
-							? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-							: <button onClick={() => { props.follow(u.id) }}>Follow</button>
+							? <button onClick={() => { 
+									axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+							      withCredentials: true,
+							      headers: {
+							      	'API-KEY': 'сгенерированный-ключ'	// i`m not authorized
+							      }
+							    })
+						      .then(response => {
+						        // this.props.toggleIsFetching(false)
+						        if (response.data.resultCode == 0) {
+						        	props.unfollow(u.id) 
+						        }
+						      })
+							}}>Unfollow</button>
+							: <button onClick={() => { 
+									axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+							      withCredentials: true,
+							      headers: {
+							      	'API-KEY': 'сгенерированный-ключ'	// i`m not authorized
+							      }
+							    })
+						      .then(response => {
+						        // this.props.toggleIsFetching(false)
+						        if (response.data.resultCode == 0) {
+						        	props.follow(u.id) 
+						        }
+						      })
+							}}>Follow</button>
 						}
 					</div>
 				</span>
