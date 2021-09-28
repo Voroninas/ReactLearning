251262ed48231eae63f7b5069821b926cfac611b
Sorokinas <KunoMan1333@yandex.ru>
2021-09-28 16:37:05 +0300

    мелкие правки, изменил размер картинок во весь экран

--- src/App.js
+++ src/App.js
@@ -26,8 +26,8 @@ function App() {
 
         <Route path='/menu'><Menu /></Route>
         <Route path='/news'><GridExample /></Route>
-        <Route path='/messages'><img style={{ width: "900px" }} src="http://swiatgry.pl/media/tapety/974/you-are-empty-1920x1080.jpg" /></Route>
-        <Route path='/settings'><img style={{ width: "900px" }} src="https://avatars.mds.yandex.net/get-zen_doc/1857055/pub_5d792ba9fbe6e73d6415732f_5d792c1934808200ad4745e2/scale_1200" /></Route>
+        <Route path='/messages'><img style={{ width: "100%" }} src="https://cdn.promodj.com/afs/c51f43e01bd7032aa206135ca8cc8ee312:resize:2000x2000:same:c08837" /></Route>
+        <Route path='/settings'><img style={{ width: "100%" }} src="https://avatars.mds.yandex.net/get-zen_doc/1857055/pub_5d792ba9fbe6e73d6415732f_5d792c1934808200ad4745e2/scale_1200" /></Route>
       </div>
     </div>
   );
