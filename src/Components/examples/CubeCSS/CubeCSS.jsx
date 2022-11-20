34a90d6dda34f75fe0def6e227b0d1238480c85c
Sorokinas <KunoMan1333@yandex.ru>
2022-11-20 17:43:56 +0300

    подправил куб CSS, но работаю через обращение к document

--- src/Components/examples/CubeCSS/CubeCSS.jsx
+++ src/Components/examples/CubeCSS/CubeCSS.jsx
@@ -1,44 +1,49 @@
-// https://youtu.be/fmSIHr3WKxk?t=357
-import React from 'react';
+// https://youtu.be/fmSIHr3WKxk?t=357  
+import React, { useState, useEffect } from 'react';
 import s from './CubeCSS.module.css';
 
+// maybe i should add some animation 
+// like spinning when keys not using
 const CubeCSS = () => {
-  // ref is not the best option, 
-  // probably should use hook or componentDidMount 
-  (() => {
-      let rotateX = 0
-      let rotateY = 0
-      document.onkeydown = (e) => {
-        if(e.keyCode===37) rotateY -=4
-        else if(e.keyCode===38) rotateX -=4
-        else if(e.keyCode===39) rotateY +=4
-        else if(e.keyCode===40) rotateX +=4
-        
-        ref.current.style.transform = 
-        'rotateX(' + rotateX + 'deg)' + 'rotateY(' + rotateY + 'deg)';
-      }
-      /*document.querySelector('.cube').style.transform = 
-      'rotateX(' + rotateX + 'deg)' + 'rotateY(' + rotateY + 'deg)';*/
-    })()
+  const [rotateX, setRotateX] = useState(0);
+  const [rotateY, setRotateY] = useState(0);
 
-  let ref = React.createRef()
+  useEffect(() => {
+    //read what key was used and reacting properly
+    document.onkeydown = (e) => {
+      if (e.keyCode === 37) setRotateY(rotateY - 2)
+      else if (e.keyCode === 38) setRotateX(rotateX - 2)
+      else if (e.keyCode === 39) setRotateY(rotateY + 2)
+      else if (e.keyCode === 40) setRotateX(rotateX + 2)
 
-  let reset = ()=> {
-    ref.current.style.transform = "rotateX(0deg) rotateY(0deg)"
-    /*rotateX = 0
-    rotateY = 0*/
+      rotateCube(rotateX, rotateY)
+    }
+  });
+
+  let rotateCube = (rotateX, rotateY) => {
+    document.querySelector('.' + s.cube).style.transform =
+      'rotateX(' + rotateX + 'deg)' + 'rotateY(' + rotateY + 'deg)';
   }
 
-  return <><div className={s.cubeContainer}>
-    <div className={s.cube} onClick={reset} ref={ref}>   {/*delete ref*/}
-      <div className={`${s.side} ${s.front}`}>front</div>
-      <div className={`${s.side} ${s.back}`}>back</div>
-      <div className={`${s.side} ${s.right}`}>right</div>
-      <div className={`${s.side} ${s.left}`}>left</div>
-      <div className={`${s.side} ${s.top}`}>top</div>
-      <div className={`${s.side} ${s.bottom}`}>bottom</div>
-    </div>
-  </div></>
+  //just reset cube position and all start again
+  let reset = () => {
+    rotateCube(0, 0)
+    setRotateX(0)
+    setRotateY(0)
+  }
+
+  return <>
+    Use arrows on keyboard to move cube. Click on cube to reset.
+    <div className={s.cubeContainer}>
+      <div className={s.cube} onClick={reset} >
+        <div className={`${s.side} ${s.front}`}>front</div>
+        <div className={`${s.side} ${s.back}`}>back</div>
+        <div className={`${s.side} ${s.right}`}>right</div>
+        <div className={`${s.side} ${s.left}`}>left</div>
+        <div className={`${s.side} ${s.top}`}>top</div>
+        <div className={`${s.side} ${s.bottom}`}>bottom</div>
+      </div>
+    </div></>
 }
 
 export default CubeCSS;
