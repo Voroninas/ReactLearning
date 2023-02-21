070ab71d335d749508f96437a78955e26a8b17fb
Sorokinas <KunoMan1333@yandex.ru>
2023-02-22 00:44:44 +0300

    мелкие эстетические правки

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom'
 
 const Header = (props) => {
   return <header className={s.header}>
-    {/*eslint-disable-next-line*/}
-    <img src='https://i.pinimg.com/736x/4c/70/63/4c7063212792902ae0d0d517a30aa1ac.jpg' />
+    <img src="%PUBLIC_URL%/favicon.ico" alt={"logo"}/>
+    {/*{<img src='https://i.pinimg.com/736x/4c/70/63/4c7063212792902ae0d0d517a30aa1ac.jpg'  alt={"logo"}/>}*/}
 
     <div className={s.loginBlock}>
       {props.isAuth ?
