1aa89769f4417c34001f9f22b313af894ee74e2c
Sorokinas <KunoMan1333@yandex.ru>
2023-07-16 21:19:54 +0300

    CubeCSS переделан на модульный CSS вариант, добавлен контроль стрелками, пока через ссылку ref

--- src/Components/examples/CubeCSS/CubeCSS.jsx
+++ src/Components/examples/CubeCSS/CubeCSS.jsx
@@ -1,33 +1,80 @@
-// https://youtu.be/fmSIHr3WKxk?t=357
-import React from 'react';
-import s from './CubeCSS.css';
-// todo переделать на модульный css (дописать слово модуль в название файла css и поменять обращение к сss на className={s.item} в файле jsx)
+// https://youtu.be/fmSIHr3WKxk?t=357  
+import React, { useState, useEffect, useRef } from 'react';
+import s from './CubeCSS.module.css';
 
 const CubeCSS = () => {
-/*  // i`m too tired for this shet, it`s not working yet
-  (function(){
-      let rotateX = 0
-      let rotateY = 0
-      document.onkeydown = function(e){
-        if(e.keyCode===37) rotateY -=4
-        else if(e.keyCode===38) rotateX -=4
-        else if(e.keyCode===39) rotateY -=4
-        else if(e.keyCode===40) rotateX -=4
-      }
-      document.querySelector('.cube').style.transform = 
-      'rotateX(' + rotateX + 'deg)' + 'rotateY(' + rotateY + 'deg)';
-    })()*/
+  const ref = useRef();
+  const [rotateX, setRotateX] = useState(0);
+  const [rotateY, setRotateY] = useState(0);
 
-  return <><div className="cubeContainer">
-    <div className="cube">
-      <div className="side front">front</div>
-      <div className="side back">back</div>
-      <div className="side right">right</div>
-      <div className="side left">left</div>
-      <div className="side top">top</div>
-      <div className="side bottom">bottom</div>
+// keyCode считается устаревшим, вместо них event.code и event.key Свойство key объекта события позволяет получить символ, а свойство code – «физический код клавиши».
+/*  // надо потестить
+let arrowKeysHandler = (e) => {
+    console.log("arrowKeysHandler сработал")
+      if (e.keyCode === 37) {e.preventDefault(); setRotateY(rotateY - 2)}
+      else if (e.keyCode === 38) {e.preventDefault(); setRotateX(rotateX - 2)}
+      else if (e.keyCode === 39) {e.preventDefault(); setRotateY(rotateY + 2)}
+      else if (e.keyCode === 40) {e.preventDefault(); setRotateX(rotateX + 2)}
+      console.log("onkeydown сработал", e.keyCode)
+  }
+  document.addEventListener("keydown", arrowKeysHandler, false); */
+  /*// аналог componentWillUnmount
+  useEffect(() => {
+    return () => {
+      // возвращаем реакцию на клавиши обратно
+      document.removeEventListener("keydown", arrowKeysHandler, false);
+      console.log("componentWillUnmount");
+    };
+  });*/
+/*}, []);*/
+
+/*// я ничего не понимаю, оказывается я делал всё в точности как положено, но тогда почему эвент листенер не убрался??? может стоит повесить его на window, а не document&
+useEffect(() => {
+    const div = innerRef.current;
+    // subscribe event
+    div.addEventListener("scroll", handleOnScroll);
+    return () => {
+      // unsubscribe event
+      div.removeEventListener("scroll", handleOnScroll);
+    };
+  }, []);
+  */
+  useEffect(() => {
+    //read what key was used and reacting properly
+    document.onkeydown = (e) => {
+      console.log("onkeydown сработал", e.keyCode)
+      if (e.keyCode === 37) {e.preventDefault(); setRotateY(rotateY - 2)}
+      else if (e.keyCode === 38) {e.preventDefault(); setRotateX(rotateX - 2)}
+      else if (e.keyCode === 39) {e.preventDefault(); setRotateY(rotateY + 2)}
+      else if (e.keyCode === 40) {e.preventDefault(); setRotateX(rotateX + 2)}
+    }
+      rotateCube(rotateX, rotateY)
+  });
+
+  let rotateCube = (rotateX, rotateY) => {
+    ref.current.style.transform = 'rotateX(' + rotateX + 'deg)' + 'rotateY(' + rotateY + 'deg)';
+    /*document.querySelector('.' + s.cube).style.transform = 'rotateX(' + rotateX + 'deg)' + 'rotateY(' + rotateY + 'deg)'; // устаревший способ*/
+  }
+  
+  let reset = () => {  //just reset cube position and all start again
+    rotateCube(0, 0)
+    setRotateX(0)
+    setRotateY(0)
+  }
+
+  return <>
+    Use arrows on keyboard to move cube. Click on cube to reset. (Arrows will work only if cube not moving, otherwise this function need enable in code)
+    <div className={s.cubeContainer}>
+      <div className={s.cube} ref={ref} onClick={reset} >
+        <div className={`${s.side} ${s.front}`}>front</div>
+        <div className={`${s.side} ${s.back}`}>back</div>
+        <div className={`${s.side} ${s.right}`}>right</div>
+        <div className={`${s.side} ${s.left}`}>left</div>
+        <div className={`${s.side} ${s.top}`}>top</div>
+        <div className={`${s.side} ${s.bottom}`}>bottom</div>
+      </div>
     </div>
-  </div></>
+    </>
 }
 
 export default CubeCSS;
