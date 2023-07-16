d6c6da93e5cb0cbc18abfdd5659fc75bc34ab2c8
Sorokinas <KunoMan1333@yandex.ru>
2023-07-16 20:48:48 +0300

    компонент хедер упрощен и его цвет изменен

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import s from './Header.module.css';
-import { NavLink } from 'react-router-dom'
 
 const Header = (props) => {
   return <header className={s.header}>
-    {/*eslint-disable-next-line*/}
-    <img src='https://i.pinimg.com/736x/4c/70/63/4c7063212792902ae0d0d517a30aa1ac.jpg' />
+    
+    <img src='https://i.pinimg.com/736x/4c/70/63/4c7063212792902ae0d0d517a30aa1ac.jpg' alt={"logo"} />
 
-    <div className={s.loginBlock}>
-      {props.isAuth ?
-        props.login :
-        <NavLink to={'/login'}>Login</NavLink>
-      }
-    </div>
   </header>
 }
 
