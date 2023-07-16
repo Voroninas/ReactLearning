2960ae3cd52066ff84444582b751ee01dbc4b4ca
Sorokinas <KunoMan1333@yandex.ru>
2023-07-16 22:01:58 +0300

    компонент футер

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+import styles from './Footer.module.css'
+
+const Footer = ()=>{
+  return <div className={styles.footer}>Этот проект сделан для тестов с SVG и версткой</div>
+}
+export default Footer
